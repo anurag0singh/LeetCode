2a7b7a1531e67316c303735f25b696ffe754804b
Anurag Singh <71708571+anurag0singh@users.noreply.github.com>
2022-02-07 13:49:18 +0530

    Time: 1 ms (79.22%), Space: 44.1 MB (5.78%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,26 +14,19 @@
  * }
  */
 class Solution {
-    static void helper(TreeNode root, Map<Integer,List<Integer>> map, int level){
+    static void helper(HashMap<Integer, List<Integer>> map, TreeNode root, int level){
         if(root == null) return;
-        if(map.containsKey(level)){
-            List<Integer> tmp = map.get(level);
-            tmp.add(root.val);
-            map.put(level, tmp);
-        }
-        else{
-            List<Integer> tmp = new ArrayList<>();
-            tmp.add(root.val);
-            map.put(level, tmp);
-        }
-        helper(root.left, map, level + 1);
-        helper(root.right, map, level + 1);
+        List<Integer> tmp = map.getOrDefault(level, new ArrayList<>());
+        tmp.add(root.val);
+        map.put(level, tmp);
+        helper(map, root.left, level + 1);
+        helper(map, root.right, level + 1);
     }
     
     public List<List<Integer>> levelOrder(TreeNode root) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
         List<List<Integer>> list = new ArrayList<>();
-        helper(root, map, 0);
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        helper(map, root, 0);
         int level = 0;
         while(map.containsKey(level)) list.add(map.get(level++));
         return list;
